1c8ff51fe6435f9b80cd5a0266480cf11583b5cb
Aya Alsakkaf <aya.alsakkaf@ccse.ku.edu.kw>
2024-01-15 17:49:03 +0300

    API Endpoint

---
+++ app.js
@@ -0,0 +1,12 @@
+express = require("express");
+data = require("./data.js");
+app = express();
+const PORT = 8000;
+
+app.get("/api/products", (request, response) => {
+  return response.json(data);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
